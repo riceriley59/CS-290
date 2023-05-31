2c828790ca6b36d2d5eced5fb3e3ec783deeed06
Riley Rice <riceriley59@gmail.com>
2023-05-31 15:31:26 -0700

    basic setup

--- HW8/server.js
+++ HW8/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+
 const app = express()
 
 app.use(express.static("public"));
 
-app.listen(3000, () => {console.log("started server")});
+app.listen(3000, () => {console.log(`started server on port: 3000`)});
