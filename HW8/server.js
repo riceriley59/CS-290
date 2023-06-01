97b283d35547ed121b88fe2518c6d866194a0dc9
Riley Rice <riceriley59@gmail.com>
2023-06-01 16:21:37 -0700

    got rid of unnecessary package

--- HW8/server.js
+++ HW8/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
