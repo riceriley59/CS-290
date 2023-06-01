9c0ed63fd80fd7bf8ab777b157e86fa86e1726d6
Riley Rice <riceriley59@gmail.com>
2023-06-01 16:19:01 -0700

    fixed error with serving static JS files

--- HW8/public/add_song/addSong.js
+++ HW8/public/add_song/addSong.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("addBtn").addEventListener("click", () => {
-        console.log("click");
+        
     });
 });
-
-console.log("loaded");
--- HW8/server.js
+++ HW8/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express()
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(cors());
 
